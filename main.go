0a297b9c20fa9454a3fb9a7f3f3105ec4907e29c
Akira Otaki <ohatakky@gmail.com>
2020-10-07 11:23:46 +0900

    fix

--- main.go
+++ main.go
@@ -13,13 +13,6 @@ import (
 	"github.com/ohatakky/github-trending/pkg/tweet"
 )
 
-const (
-	worker   = 1
-	duration = 10
-)
-
-var twitter = tweet.New(os.Getenv("API_KEY"), os.Getenv("API_SECRET"), os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_TOKEN_SECRET"))
-
 func getIP(r *http.Request) string {
 	forwarded := r.Header.Get("X-FORWARDED-FOR")
 	if forwarded != "" {
@@ -49,13 +42,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 
-	wg := &sync.WaitGroup{}
-	wg.Add(worker)
 	jobs := make(chan trending.Item)
+	twitter := tweet.New(os.Getenv("API_KEY"), os.Getenv("API_SECRET"), os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_TOKEN_SECRET"))
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		for j := range jobs {
-			time.Sleep(duration * time.Minute)
+			time.Sleep(10 * time.Minute)
 			if twitter.Tweet(j.Link) != nil {
 				log.Println(err)
 			}
