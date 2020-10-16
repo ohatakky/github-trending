4a2d5259fe12357837d1c04d7bca9957c0312d5b
Akira Otaki <ohatakky@gmail.com>
2020-10-17 02:04:33 +0900

    fix

--- go.mod
+++ go.mod
@@ -7,7 +7,9 @@ require (
 	github.com/ChimeraCoder/tokenbucket v0.0.0-20131201223612-c5a927568de7 // indirect
 	github.com/PuerkitoBio/goquery v1.5.1
 	github.com/azr/backoff v0.0.0-20160115115103-53511d3c7330 // indirect
+	github.com/carlescere/scheduler v0.0.0-20170109141437-ee74d2f83d82
 	github.com/dustin/go-jsonpointer v0.0.0-20160814072949-ba0abeacc3dc // indirect
 	github.com/dustin/gojson v0.0.0-20160307161227-2e71ec9dd5ad // indirect
 	github.com/garyburd/go-oauth v0.0.0-20180319155456-bca2e7f09a17 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,71 +2,48 @@ package main
 
 import (
 	_ "expvar"
-	"fmt"
 	"log"
-	"net/http"
 	"os"
+	"runtime"
 	"sync"
 	"time"
 
+	"github.com/carlescere/scheduler"
 	"github.com/ohatakky/github-trending/pkg/trending"
 	"github.com/ohatakky/github-trending/pkg/tweet"
 )
 
-func getIP(r *http.Request) string {
-	forwarded := r.Header.Get("X-FORWARDED-FOR")
-	if forwarded != "" {
-		return forwarded
-	}
-	return r.RemoteAddr
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	ip := getIP(r)
-	if ip != "0.1.0.1" {
-		w.WriteHeader(http.StatusBadRequest)
-		log.Println(fmt.Sprintf("invalid IP: %s", ip))
-		return
-	}
-	if r.Header.Get("X-Appengine-Cron") != "true" {
-		w.WriteHeader(http.StatusBadRequest)
-		log.Println(fmt.Sprintf("invalid access: not appengine cron"))
-		return
-	}
-
-	cli := trending.New()
-	items, err := cli.Read()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Printf("fetch trending is failed: %s\n", err.Error())
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-
-	jobs := make(chan trending.Item)
-	twitter := tweet.New(os.Getenv("API_KEY"), os.Getenv("API_SECRET"), os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_TOKEN_SECRET"))
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for j := range jobs {
-			time.Sleep(10 * time.Minute)
-			if twitter.Tweet(j.Link) != nil {
-				log.Printf("post tweet is failed: %s\n", err.Error())
-			}
-		}
-	}()
-
-	for _, item := range items {
-		jobs <- *item
-	}
-
-	close(jobs)
-	wg.Wait()
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	log.Println("running...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	job := func() {
+		cli := trending.New()
+		items, err := cli.Read()
+		if err != nil {
+			log.Fatalf("fetch trending is failed: %s\n", err.Error())
+		}
+
+		queue := make(chan trending.Item)
+		twitter := tweet.New(os.Getenv("API_KEY"), os.Getenv("API_SECRET"), os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_TOKEN_SECRET"))
+		wg := &sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for item := range queue {
+				if twitter.Tweet(item.Link) != nil {
+					log.Printf("post tweet is failed: %s\n", err.Error())
+				}
+				time.Sleep(4 * time.Minute)
+			}
+		}()
+
+		for _, item := range items {
+			queue <- *item
+		}
+
+		close(queue)
+		wg.Wait()
+	}
+
+	scheduler.Every().Day().At("08:00").Run(job)
+
+	runtime.Goexit()
 }
